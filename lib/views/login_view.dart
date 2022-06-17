0d1f6102084b3c320f124af8646dff4dcc4b9ce5
FLAMBEAU Aristide <flambeaua@gmail.com>
2022-06-17 17:02:31 +0200

    step-8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
+import 'package:mynotes/views/verify_email.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -57,8 +58,16 @@ class _LoginViewState extends State<LoginView> {
             try {
               await FirebaseAuth.instance
                   .signInWithEmailAndPassword(email: email, password: password);
-              Navigator.of(context)
-                  .pushNamedAndRemoveUntil(notesRoute, (route) => false);
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                // User's email is verified
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil(notesRoute, (route) => false);
+              } else {
+                // User's email is NOT verified
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    verifyEmailRoute, (route) => false);
+              }
             } on FirebaseAuthException catch (e) {
               await showErrorDialog(
                 context,
